ffb7a7af5ce018c91dfd5017cea31c1fb92bf84a
Bryan Hill <bryan.hill@shaw.ca>
2018-08-06 20:16:57 -0600

    Add problem #3

---
+++ project_euler/solutions/problem3.py
@@ -0,0 +1,14 @@
+def get_answer(number=600_851_475_143):
+    """
+    This algorithm continually divides number evenly by i until i > number.
+    This ensures that it will divide all prime numbers less than its largest prime factor,
+    leaving you with the the largest prime factor
+    This algorithm works well if the largest prime factor of number is relatively small (<10_000_000).
+    :return: The largest prime factor of number
+    """
+    i = 2
+    while i < number:
+        while number % i == 0:
+            number //= i
+        i += 1
+    return number
