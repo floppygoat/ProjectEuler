714afdc1de570709bc9121d66b123e850c9cbe92
Bryan Hill <bryan.hill@shaw.ca>
2018-08-11 13:12:52 -0600

    Added problem 11

--- project_euler/library.py
+++ project_euler/library.py
@@ -123,11 +123,11 @@ def n_choose_k(n, k):
     return reduce(lambda i, j: i * j, range(n - k + 1, n + 1)) // reduce(lambda i, j: i * j, range(1, k + 1))
 
 
-def file_to_matrix(filename, seperator):
+def file_to_matrix(filename, separator):
     f = open(filename, "r")
     strings = f.readlines()
     f.close()
     array = []
     for x in range(0, len(strings)):
-        array.append(list(map(int, strings[x].split(seperator))))
+        array.append(list(map(int, strings[x].split(separator))))
     return array
