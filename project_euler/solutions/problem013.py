dcdfcc3743cce65a94cd3ea5c2cdf49f2e980a19
Bryan Hill <bryan.hill@shaw.ca>
2018-08-16 23:48:24 -0600

    Add Problem 500

--- project_euler/solutions/problem013.py
+++ project_euler/solutions/problem013.py
@@ -1,9 +1,7 @@
 def get_answer(filename="text_files/problem013.txt", number_of_digits=10):
     """
     Due to Python's arbitrary precision, you can just explicitly add all rows of filename together
-    :param filename:
-    :param number_of_digits:
-    :return:
+    :return: The sum of all numbers (with each number on a newline) in file filename
     """
     f = open(filename, "r")
     strings = f.readlines()
