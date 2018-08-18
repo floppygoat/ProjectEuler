5b400fc938b58fd3e23b0ca5e0ab13a97cfa1bad
Bryan Hill <bryan.hill@shaw.ca>
2018-08-17 22:56:08 -0600

    Optimized problem 14

--- project_euler/solutions/problem014.py
+++ project_euler/solutions/problem014.py
@@ -5,13 +5,20 @@ def get_answer(bound=1_000_000):
     max_count, longest = 0, 0
     sequence = [0] * bound
     sequence[0] = 1
+
     for i in range(bound // 2, bound):
         x, count = i, 0
         seq = []
         while True:
+            # If we have calculated the value before:
             if x < bound and sequence[x - 1] != 0:
+
+                # If the starting value has already been calculated, there must be a longer sequence less than x
+                # (as i -> 1 is a sub-sequence of x < i Collatz sequence).
                 if i == x:
                     break
+
+                # Update sequence list
                 c = sequence[x - 1]
                 for y in reversed(seq):
                     if y % 2 == 0:
@@ -23,6 +30,8 @@ def get_answer(bound=1_000_000):
                 count += sequence[x - 1]
                 sequence[i - 1] = count
                 break
+
+            # Else, remember the next value of this chain
             seq.append(x)
             if x % 2 == 0:
                 x //= 2
@@ -30,6 +39,7 @@ def get_answer(bound=1_000_000):
             else:
                 x = ((3 * x) + 1) // 2
                 count += 2
+
         if count > max_count:
             max_count = count
             longest = i
