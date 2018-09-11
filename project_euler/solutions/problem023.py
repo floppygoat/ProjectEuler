9d4f12d8eb4ee323c6f0cdba0f90301bcd388587
Bryan Hill <bryan.hill@shaw.ca>
2018-09-10 22:46:17 -0700

    Added solutions and comments

--- project_euler/solutions/problem023.py
+++ project_euler/solutions/problem023.py
@@ -1,31 +1,21 @@
-import time
-
-
 def get_answer(limit=28_124):
-    start_time = time.perf_counter()
+    """
+    It first finds the sum of all its divisors (similar to problem 21).
+    Afterwards, we build up a set up abundant numbers while checking
+    if each number can be subtracted by an abundant number to obtain another
+    abundant number.
+    :return: The sum of all numbers that cannot be expressed as the sum of two non-abundant numbers.
+    """
     limit = min(limit, 28_124)
     summation = 0
     sum_divisors = [0] * limit
     for i in range(1, limit // 2):
         for j in range(2 * i, limit, i):
             sum_divisors[j] += i
-    abundant = [False] * limit
+    abundant = set()
     for i in range(1, limit):
         if sum_divisors[i] > i:
-            abundant[i] = True
-    runtime = time.perf_counter() - start_time
-    print("Elapsed time:", runtime, "seconds")
-    for i in range(1, limit):
-        check = False
-        if i % 2 == 0:
-            x, y = i // 2, i // 2
-        else:
-            x, y = i // 2, i // 2 + 1
-        while not check and x > 0:
-            if abundant[x] and abundant[y]:
-                check = True
-            x -= 1
-            y += 1
-        if not check:
+            abundant.add(i)
+        if not any((i - a in abundant) for a in abundant):
             summation += i
     return summation
